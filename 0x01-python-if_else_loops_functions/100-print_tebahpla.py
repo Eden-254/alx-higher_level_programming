430a46dd2ee3989ec854ca2174cb4ea13aaf0577
Eden-254 <otandoeden@gmail.com>
2023-01-04 13:14:00 -0800

    might be fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for c in reversed(range(97, 123)):
     if (c % 2 == 0):
-        print('{:c}'.format(i), end='')
+        print('{:c}'.format(c), end='')
     else:
-        print('{:c}'.format(i - 32), end='')
+        print('{:c}'.format(c - 32), end='')
